2758ba81045f3edaa071a0a4177e79b93854f6e4
Arabian619 <www.arabiandollar152@gmail.com>
2024-07-29 21:48:20 -0700

    gather data 3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,19 @@ gather employee data from API
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
-
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
 
     employee_id = sys.argv[1]
     user_response = requests.get(url + "users/{}".format(employee_id))
     user = user_response.json()
-    param = {"userId": employee_id}
-    todo_response = requests.get(url + "todos", param = param)
-    todo_response = todo_reponse.json()
+    params = {"userId": employee_id}
+    todo_response = requests.get(url + "todos", params = params)
+    todos = todo_response.json()
     complete = []
 
     for todo in todos:
-        if tod.get("complete") is True:
+        if todo.get("complete") is True:
             complete.append(todo.get("title"))
 
     print("Employee {} is done with tasks({}/{})".format(user.get("name"), len(complete), len(todos)))
